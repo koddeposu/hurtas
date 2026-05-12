6863d5872a1a91ed2551b450cc7a6dbba8f5f841
YasinHarmanci <yasinharmanci52@gmail.com>
2026-05-12 13:02:00 +0300

    add

--- next.config.ts
+++ next.config.ts
@@ -1,31 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/prefabrik-evler/tek-katli",
-        destination: "/prefabrik-evler/tek-katli-prefabrik-evler",
-        permanent: true,
-      },
-      {
-        source: "/prefabrik-evler/cift-katli",
-        destination: "/prefabrik-evler/cift-katli-prefabrik-evler",
-        permanent: true,
-      },
-    ];
-  },
   images: {
     formats: ["image/avif", "image/webp"],
     deviceSizes: [320, 480, 560, 640, 768, 1024, 1280],
     imageSizes: [280, 300, 320, 360, 400],
     qualities: [10, 20, 30, 40, 50, 55, 58, 60, 70, 75, 80, 90, 100],
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "pub-dedb7d7baa404113b58cbbd099fd1e3f.r2.dev",
-        pathname: "/**",
-      },
       {
         protocol: "https",
         hostname: "cdn.hurtasbeton.com",
